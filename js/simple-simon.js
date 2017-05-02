ef59666e35102504c390f21394bf16e153ba7587
DelMonroe <delenor@gmail.com>
2017-05-02 10:22:01 -0500

    Completed visual aspect of game console, with minimum functionality

--- js/simple-simon.js
+++ js/simple-simon.js
@@ -1,3 +1,47 @@
 /**
  * Created by DelMonroe on 5/1/17.
  */
+
+$(document).ready(function () {
+    "use strict";
+
+    /* ======================== Global Variables ==================== */
+    console.log("ready");
+    var panelPatternCounter = 0;
+    var gamemode = false;
+    var level = 1;
+    var gamePanel = [];
+
+
+
+
+
+    /* ======================== Toggle the start button from green to red ==================== */
+
+    $('#start').click(function () {
+        $('.img-circle').toggleClass('stop');
+    });
+
+
+
+/* ======================== Logic for catching user sequence into an array ==================== */
+
+    function newMemory() {
+        var temp_mem = Math.floor((Math.random() * 4) + 1);
+        switch(temp_mem) {
+            case 1:
+                gamePanel.push("Green");
+                break;
+            case 2:
+                gamePanel.push("Red");
+                break;
+            case 3:
+                gamePanel.push("Yellow");
+                break;
+            case 4:
+                gamePanel.push("Blue");
+                break;
+        }
+    }
+
+});
