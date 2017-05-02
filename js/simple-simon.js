e5c7941a120ba27deec7809112ddf64d86af3dd2
DelMonroe <delenor@gmail.com>
2017-05-02 15:48:33 -0500

    Continuing to work on logic

--- js/simple-simon.js
+++ js/simple-simon.js
@@ -11,6 +11,48 @@ $(document).ready(function () {
     var gamemode = false;
     var level = 1;
     var gamePanel = [];
+    var mytime;
+
+
+
+    /* ======================== Initialize counter with double hyphens ==================== */
+    $('#counter').html('--');
+
+
+
+
+    /* ======================== Temporary Counter ==================== */
+
+
+
+    // add a zero in front of numbers<10
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    function startTime() {
+        var t, S;
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        // Turn seconds into ticker just to test round count
+
+        s = checkTime(s);
+        $('#counter').html(s);
+        mytime = setTimeout(function() {
+            startTime();
+        }, 500);
+    }
+
+    function stopTime() {
+        clearTimeout(mytime);
+
+    }
+
 
 
 
@@ -18,9 +60,21 @@ $(document).ready(function () {
 
     /* ======================== Toggle the start button from green to red ==================== */
 
+
     $('#start').click(function () {
+        stopTime();
+        $(this).effect("highlight", {}, 100);
         $('.img-circle').toggleClass('stop');
+        if ($('.img-circle').hasClass('stop')) {
+            startTime();
+        } else {
+            stopTime();
+            $('#counter').html('--');
+        }
+
     });
+
+
 
 
 
@@ -43,5 +97,10 @@ $(document).ready(function () {
                 break;
         }
     }
+
+
+    $( document ).click(function() {
+        $( "#red" ).toggle( "highlight" );
+    });
 
 });
